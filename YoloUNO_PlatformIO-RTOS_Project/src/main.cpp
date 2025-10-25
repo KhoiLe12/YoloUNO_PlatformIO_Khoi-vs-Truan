e503b06997ab319afc1c99f7b137c677ac76b1ea
KhoiLe12 <107261238+KhoiLe12@users.noreply.github.com>
2025-10-25 14:05:26 +0700

    Initialize I2C and update task management
    Initialized I2C and created synchronization objects before task creation. Updated task creation for LED blinking and temperature/humidity monitoring.

--- YoloUNO_PlatformIO-RTOS_Project/src/main.cpp
+++ YoloUNO_PlatformIO-RTOS_Project/src/main.cpp
@@ -1,23 +1,46 @@
 #include "global.h"
-
 #include "led_blinky.h"
 #include "neo_blinky.h"
 #include "temp_humi_monitor.h"
 #include "mainserver.h"
 #include "tinyml.h"
+#include <Wire.h>
+#include <freertos/semphr.h>
 
-
+// define shared mode and handles
+volatile LedMode g_ledMode = LED_OFF;
+SemaphoreHandle_t xLedModeMutex = NULL;
+SemaphoreHandle_t xLedModeChange = NULL;
 
 void setup() {
   Serial.begin(115200);
+  vTaskDelay(pdMS_TO_TICKS(100));
 
-  xTaskCreate( led_blinky, "Task LED Blink" ,2048  ,NULL  ,2 , NULL);
-  xTaskCreate( neo_blinky, "Task NEO Blink" ,2048  ,NULL  ,2 , NULL);
-  xTaskCreate( temp_humi_monitor, "Task TEMP HUMI Monitor" ,2048  ,NULL  ,2 , NULL);
-  xTaskCreate( main_server_task, "Task Main Server" ,2048  ,NULL  ,2 , NULL);
-  xTaskCreate( tiny_ml_task, "Tiny ML Task" ,2048  ,NULL  ,2 , NULL);
+  const int SDA_PIN = 11;
+  const int SCL_PIN = 12;
+  pinMode(SDA_PIN, INPUT_PULLUP);
+  pinMode(SCL_PIN, INPUT_PULLUP);
+  Wire.begin(SDA_PIN, SCL_PIN);
+  Wire.setClock(100000);
+  Serial.println("I2C Initialized.");
+
+  // create synchronization objects BEFORE creating tasks
+  xLedModeMutex = xSemaphoreCreateMutex();
+  xLedModeChange = xSemaphoreCreateBinary();
+  if (xLedModeMutex == NULL || xLedModeChange == NULL) {
+    Serial.println("Failed to create LED semaphores");
+    while (1) vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+
+  // create tasks
+  xTaskCreate(temp_humi_monitor, "Task TEMP HUMI Monitor", 2048, NULL, 2, NULL);
+  xTaskCreate(led_blinky, "Task LED Blink", 2048, NULL, 2, NULL);
+  // xTaskCreate(neo_blinky, "Task NEO Blink", 2048, NULL, 2, NULL);
+  // xTaskCreate(main_server_task, "Task Main Server", 2048, NULL, 2, NULL);
+  xTaskCreate(tiny_ml_task, "Tiny ML Task", 4096, NULL, 2, NULL);
 }
 
-void loop() {
-  
+void loop() { 
+
 }
+
